cadcc6ed90c4ef2676dd453c7096e2e1acf86987
learningreac <learningreact2020@gmail.com>
2021-11-18 09:15:29 -0800

    adjust the imge size in pdf to avoid strentching

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
 const jsPDF = window.jspdf.jsPDF;
 const doc = new jsPDF();
-
+//doc.text("Hello world2! this is the Title", 10, 10);
+const docWidth = doc.internal.pageSize.getWidth();
+const docHeight = doc.internal.pageSize.getHeight();
+//console.log('doc', docWidth, docHeight)
 
 
 class Album {
 	constructor(containerElement, index, file) {
-		
-		const image = document.createElement('img');
+		const image = new Image();
 		const reader = new FileReader;
-		reader.addEventListener('load', function(){
-			 image.src = reader.result;
-			 doc.addImage( image.src, "JPEG", 15, 40, 180, 180);
-			 },false);
-		if (file) {
-		    reader.readAsDataURL(file);
-		};
-		
-		image.dataset.index = index;
-		image.title= file.name;
-		containerElement.appendChild(image);
+		let imgWidth, imgHeight, ratio;
 
-		//console.log('image', image)
+		reader.onload = function () {
+			image.onload = function () {
+				imgWidth = image.width;
+				imgHeight = image.height;
+				ratio = imgWidth / imgHeight;
+				//console.log('onload', imgWidth, imgHeight, ratio);
+
+				let renderWidth = (imgWidth > docWidth) ? docWidth : imgWidth;
+				renderWidth -= 20;
+				let renderHeight = renderWidth / ratio;
+				//console.log('render', renderWidth, renderHeight);
+
+				if (index > 0) {
+					doc.addPage();
+				}
+				doc.addImage(image.src, "JPEG", 10, 20, renderWidth, renderHeight); // width unit mm
+
+			};
+
+			image.src = reader.result;
+
+		}
+		if (file) {
+			reader.readAsDataURL(file);
+		};
+
+		image.dataset.index = index;
+		image.title = file.name;
+		containerElement.appendChild(image);
 	}
-}; 
+};
 
 
 class App {
 	constructor() {
 		this.filesInfo = {};
-		
+
 		//bind event handlers
 		this._openFileDialog = this._openFileDialog.bind(this);
 		this._handleFileSelect = this._handleFileSelect.bind(this);
@@ -38,16 +58,17 @@ class App {
 
 		this.fileDialog = document.getElementById("fileDialogId");
 		this.fileDialog.addEventListener('change', this._handleFileSelect, false);
-	
+
 		this.indexArr = [];
 
 	};
 
 	_openFileDialog() {
+		console.log('btn clicked')
 		this.fileDialog.click();
 	};
 
-	_handleFileSelect(evt){
+	_handleFileSelect(evt) {
 		this.filesInfo = evt.target.files;
 		console.log(this.filesInfo);
 		this._renderAlbums();
@@ -58,13 +79,13 @@ class App {
 		const albumContainer = document.querySelector("#album-container");
 		albumContainer.innerHTML = '';
 		//for(const file of this.filesInfo) {
-		for(let i=0; i<this.filesInfo.length; i++){
+		for (let i = 0; i < this.filesInfo.length; i++) {
 			const file = this.filesInfo[i];
 			file.index = i; // should only run for the first render
 			const preview = new Album(albumContainer, i, file);
 		};
 
-		
+
 	};
 
 
@@ -74,4 +95,4 @@ class App {
 
 const app = new App();
 const geneBtn = document.querySelector("#generate");
-geneBtn.addEventListener('click', ()=>doc.save("a8.pdf"))
+geneBtn.addEventListener('click', () => doc.save("a8.pdf"))
--- pdf.js
+++ pdf.js
@@ -7,4 +7,15 @@ doc.text("Hello world!", 10, 10);
 //doc.addImage("examples/images/Octonyan.jpg", "JPEG", 15, 40, 180, 180);
 doc.text("Hello world2!", 10, 10);
 doc.save("a5.pdf");
+
+
+
+	const imgWidth, imgHeight, ratio;
+		reader.onload = function () {
+			image.onload = function (){
+				imgWidth = image.width;
+				imgHeight = image.height;
+				ratio = imgWidth/imgHeight;
+				console.log('onload',imgWidth,imgHeight,ratio);
+			};
 */
